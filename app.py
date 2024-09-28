52d32779bdb5a53aecc99e51fcad484a44c4aa33
Everhett Raman <everhett@evmbp.local>
2024-09-28 12:02:50 +0800

    Replace query engine with chat-engine.

--- app.py
+++ app.py
@@ -1,65 +1,14 @@
-from http.client import responses
-
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
-from llama_index.core.llms import ChatMessage, MessageRole
-from llama_index.core import ChatPromptTemplate
 
 
-# %%
-def get_query_engine(model_name: str, chunk_size: int = None):
-    qa_prompt_str = (
-        "Context information is below.\n"
-        "---------------------\n"
-        "{context_str}\n"
-        "---------------------\n"
-        "Given the context information and not prior knowledge, "
-        "answer the question: {query_str}\n"
-    )
-
-    refine_prompt_str = (
-        "We have the opportunity to refine the original answer "
-        "(only if needed) with some more context below.\n"
-        "------------\n"
-        "{context_msg}\n"
-        "------------\n"
-        "Given the new context, refine the original answer to better "
-        "answer the question: {query_str}. "
-        "If the context isn't useful, output the original answer again.\n"
-        "Original Answer: {existing_answer}"
-    )
-
-    # Text QA Prompt
-    chat_text_qa_msgs = [
-        ChatMessage(
-            role=MessageRole.SYSTEM,
-            content=(
-                "Always answer the question, even if the context isn't helpful."
-            ),
-        ),
-        ChatMessage(role=MessageRole.USER, content=qa_prompt_str),
-    ]
-    text_qa_template = ChatPromptTemplate(chat_text_qa_msgs)
-
-    # Refine Prompt
-    chat_refine_msgs = [
-        ChatMessage(
-            role=MessageRole.SYSTEM,
-            content=(
-                "Always answer the question, even if the context isn't helpful."
-            ),
-        ),
-        ChatMessage(role=MessageRole.USER, content=refine_prompt_str),
-    ]
-    refine_template = ChatPromptTemplate(chat_refine_msgs)
-
-    documents = SimpleDirectoryReader("data").load_data()
+def get_vector_store(model_name: str, data_dir:str, chunk_size: int = None):
+    documents = SimpleDirectoryReader(data_dir).load_data()
 
     # This code was disabled because this only works for OpenAI.
     # index=VectorStoreIndex.from_documents(documents,show_progress=True)
     # Remember! Don't use ServiceContext it was depreciated and replaced with Settings.
-
     Settings.embed_model = HuggingFaceEmbedding(
         cache_folder="embedding-model",
         model_name=model_name,
@@ -74,14 +23,20 @@ def get_query_engine(model_name: str, chunk_size: int = None):
     # Vector Store Index turns all of your text into embeddings using an API from your LLM
     index = VectorStoreIndex.from_documents(
         documents,
-        # show_progress=True,
+        show_progress=True,
     )
 
     # Returns a query engine.
-    return index.as_query_engine(
-        text_qa_template=text_qa_template,
-        refine_template=refine_template
-    )
+    # return index.as_query_engine(
+    #     text_qa_template=text_qa_template,
+    #     refine_template=refine_template
+    # )
+
+    # return index.as_chat_engine(
+    #     chat_mode="condense_question",
+    #     verbose=True
+    # )
+    return index
 
     # Returns a query engine.
     # retriever = VectorIndexRetriever(index=index,similarity_top_k=4)
@@ -92,10 +47,13 @@ if __name__ == '__main__':
     What makes Appa to pursue a career in teaching? 
     # """
     try:
-        query_engine1 = get_query_engine("BAAI/bge-base-en-v1.5")
+        vector_store = get_vector_store("BAAI/bge-base-en-v1.5", "data")
+        chat_engine = vector_store.as_chat_engine(
+                chat_mode="condense_question",
+                verbose=True
+        )
+        response = chat_engine.chat(question)
+        print(response)
         # query_engine2 = get_query_engine("sentence-transformers/all-mpnet-base-v2", 1024)
     except Exception as e:
         print("Exception: ---->", e)
-
-    response = query_engine1.query(question)
-    print(response)
