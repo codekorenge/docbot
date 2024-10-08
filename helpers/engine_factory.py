9eff74e72459e0c5407cd36803d389802abb49b7
Everhett Raman <everhett@evmbp.local>
2024-10-08 14:08:38 +0800

    Integrated retriever to chatbot engine.

--- helpers/engine_factory.py
+++ helpers/engine_factory.py
@@ -1,13 +1,20 @@
+from lib2to3.fixes.fix_input import context
+
+from llama_index.core import PromptTemplate
 from llama_index.core import VectorStoreIndex, get_response_synthesizer
+from llama_index.core.chat_engine import CondenseQuestionChatEngine, CondensePlusContextChatEngine
+from llama_index.core.llms import ChatMessage, MessageRole
 from llama_index.core.postprocessor import SimilarityPostprocessor
 from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.retrievers import VectorIndexRetriever
-
+from llama_index.core.memory import ChatMemoryBuffer
 
 class EngineFactory:
     def __init__(self):
         pass
-
+    """
+    Code was referenced from here: https://docs.llamaindex.ai/en/stable/understanding/querying/querying/
+    """
     def get_query_retriever(self,
                             index: VectorStoreIndex,
                             similarity_top_k: int,
@@ -24,6 +31,63 @@ class EngineFactory:
         # assemble query engine
         return RetrieverQueryEngine(
             retriever=retriever,
-            response_synthesizer=response_synthesizer,
+            # response_synthesizer=response_synthesizer,
             node_postprocessors=[SimilarityPostprocessor(similarity_cutoff=similarity_cutoff)]
         )
+
+    """
+    Code was referenced from: https://docs.llamaindex.ai/en/stable/module_guides/deploying/chat_engines/usage_pattern/
+    """
+    def get_context_chat_engine(self, retriever: RetrieverQueryEngine, token_limit:int) -> CondensePlusContextChatEngine:
+        memory = ChatMemoryBuffer.from_defaults(token_limit=token_limit)
+
+        # custom_prompt = PromptTemplate(
+        #     """\
+        # Given a conversation (between Human and Assistant) and a follow up message from Human, \
+        # rewrite the message to be a standalone question that captures all relevant context \
+        # from the conversation.
+        #
+        # <Chat History>
+        # {chat_history}
+        #
+        # <Follow Up Message>
+        # {question}
+        #
+        # <Standalone question>
+        # """
+        # )
+
+        # list of `ChatMessage` objects
+        # custom_chat_history = [
+        #     ChatMessage(
+        #         role=MessageRole.USER,
+        #         content="Hello assistant, we are having a insightful discussion about Paul Graham today.",
+        #     ),
+        #     ChatMessage(role=MessageRole.ASSISTANT, content="Okay, sounds good."),
+        # ]
+
+        # chat_engine = CondenseQuestionChatEngine.from_defaults(
+        #     query_engine=retriever,
+        #     condense_question_prompt=custom_prompt,
+        #     chat_history=custom_chat_history,
+        #     verbose=True,
+        # )
+
+        system_prompt=("You are a chatbot, able to have normal interactions, as well as talk about documents "
+                       "in the database. Always explain queries from a third person perspective.")
+        context_prompt= ("Here are the relevant documents for the context:\n"
+                         "{context_str}"
+                         "\nInstruction: Use the previous chat history, or the context above, to interact and help the user.")
+
+        chat_engine = CondensePlusContextChatEngine.from_defaults(
+            retriever=retriever,
+            system_prompt=system_prompt,
+            context_prompt=context_prompt,
+            memory=memory,
+            # condense_prompt=custom_prompt,
+            # condense_question_prompt=custom_prompt,
+            # chat_history=custom_chat_history,
+            verbose=True,
+        )
+
+        return chat_engine
--- tests/test_query.py
+++ tests/test_query.py
@@ -93,5 +93,47 @@ class TestEngineFactory(unittest.TestCase):
                   f"\tdocument:{node.metadata['file_name']}, "
                   f"\ttext:{node.text[0:30]} ...")
 
+    def test_get_chat_engine_with_huggingface_baai_embedding(self):
+        embedding_factory = EmbeddingFactory("embedding-model")
+
+        embedding_model = embedding_factory.get_huggingface_embedding("BAAI/bge-base-en-v1.5")
+        self.assertEqual(embedding_model.model_name, "BAAI/bge-base-en-v1.5")
+
+        factory = VectorFactory("llama3.1", embedding_model, True)
+
+        # Returns an updated global setting configuration that need to be applied when required.
+        index, settings = factory.get_vector_index("../data", 200, 10)
+        self.assertIsNotNone(index.vector_store)
+        self.assertEqual(settings.embed_model, embedding_model)
+
+        #  Manually creating an engine to assert.
+        engine = index.as_query_engine(
+            response_mode="tree_summarize",
+            verbose=True
+        )
+
+        response = engine.query("who is appa?")
+        self.assertIsNotNone(response)
+
+        engine_factory = EngineFactory()
+
+        retriever_engine = engine_factory.get_query_retriever(index, 10, 0.55)
+        chat_engine = engine_factory.get_context_chat_engine(retriever_engine, 1500)
+
+        response = chat_engine.chat("what is the name of first american president?")
+
+        self.assertIsNotNone(response)
+        print(f"response: {response}")
+
+        cnt = 0
+        for node in response.source_nodes:
+            cnt += 1
+            print(f"{cnt}."
+                  f"\tscore:{node.score}, "
+                  f"\tword-count:{len(node.text.split())}, "
+                  # f"node-meta:{node.metadata}, "
+                  f"\tdocument:{node.metadata['file_name']}, "
+                  f"\ttext:{node.text[0:30]} ...")
+
 if __name__ == '__main__':
     unittest.main()
